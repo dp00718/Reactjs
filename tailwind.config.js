397fb288511a7928a911c6e466c1d90434ee487b
dp00718 <156597537+dp00718@users.noreply.github.com>
2024-06-09 13:08:22 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/Form2.{js,jsx,ts,tsx}",
+  ],
+
+  content: [
+    "./src/**/Cart.{js,jsx,ts,tsx}",
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
